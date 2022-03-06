06057d0fa1e486917f0b2f7378cf545fa039478f
cly <you@example.com>
2022-03-06 23:49:19 +0800

    作业

---
+++ W2/w2_1/README.md
@@ -0,0 +1,7 @@
+# 使用hardhat部署Bank合约
+https://ropsten.etherscan.io/tx/0xa95a6a29e7770fadea8319e769fa447cee0cd1f0d91d828b1f76c47daa6a88d5
+# 通过 Metamask 向Bank合约转账ETH
+https://ropsten.etherscan.io/tx/0xa58c39d624f5a96c42c56eebd407b1f342217014dc55bf060fef84c8d012a0e6
+# 在Bank合约记录每个地址转账⾦额
+# 编写 Bank合约withdraw(), 实现提取出所有的 ETH
+https://ropsten.etherscan.io/tx/0x5f68836e64eb285eae3e6de327fdc2021eb8d4fff151a0cab6919e2bbeeec33f
---
+++ W2/w2_1/contracts/Bank.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Bank {
+    struct RechargeRecord {
+        //充值金额
+        uint256 amount;
+        //充值时间
+        uint256 time;
+    }
+    //用户充值金额
+    mapping(address => RechargeRecord[]) public user;
+
+    mapping(address => uint256) public userBalance;
+
+    event Recharge(address indexed addr, uint256 amount, uint256 time);
+
+    event Withdraw(address indexed addr, uint256 amount, uint256 time);
+
+    //查询充值记录
+    function getUser(address _addr)
+        public
+        view
+        returns (RechargeRecord[] memory)
+    {
+        return user[_addr];
+    }
+
+    //充值主链币
+    receive() external payable {
+        user[msg.sender].push(
+            RechargeRecord({amount: msg.value, time: block.timestamp})
+        );
+        userBalance[msg.sender] += msg.value;
+        emit Recharge(msg.sender, msg.value, block.timestamp);
+    }
+
+    //提现全部主链币,撤池子
+    function withdraw() public {
+        require(address(this).balance > 0, "Sorry, your credit is running low");
+        uint256 oldBalance = address(this).balance;
+        userBalance[msg.sender] = 0;
+        (bool successc, ) = msg.sender.call{value: oldBalance}("");
+        require(successc, "withdraw transfer failed");
+        emit Withdraw(msg.sender, oldBalance, block.timestamp);
+    }
+}
